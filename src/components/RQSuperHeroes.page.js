5972ede327dc925eead9ab9c93a15bc3240a1704
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-02-11 11:50:16 +0100

    Fetching Data with useQuery

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,24 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+// ! Funktion für den API-Call
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  // ! React Query
+  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
+};
