85ea1db090bde588dee232f368c0dd9b189ffdaa
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-02-16 06:25:38 +0100

    Axios Interceptor

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,14 +1,16 @@
 import { useQuery, useMutation, useQueryClient } from 'react-query';
-import axios from 'axios';
+import { request } from '../utils/axios-utils';
 
 // ! Fetcher-Funktion für den API-Call
 const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes');
+  // return axios.get('http://localhost:4000/superheroes');
+  return request({ url: '/superheroes' });
 };
 
 // ! Mutation-Funktion für den API-Call
 const addSuperHero = (hero) => {
-  return axios.post('http://localhost:4000/superheroes', hero);
+  // return axios.post('http://localhost:4000/superheroes', hero);
+  return request({ url: '/superheroes', method: 'post', data: hero });
 };
 
 // * Custom Hook für Abfragen
